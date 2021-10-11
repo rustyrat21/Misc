dadf679b99f04ad0e68a6a2e63ae90c44d170d92
Phil Hampton <hampton.jp@gmail.com>
2021-10-11 13:53:35 -0400

    Original Update
    Get-MSCatalogUpdate pulls Updates from MS and puts them in a Spreasdshett

---
+++ Get-MSCatalogUpdate.ps1
@@ -0,0 +1,59 @@
+$date = Get-date -Format "MM-dd-yy"
+$location = "C:\data\endpoint\$date"
+$1809 = "Win10_1809_updates.csv"
+$1903 = "Win10_1903_updates.csv"
+$1909 = "Win10_1909_updates.csv"
+$2004 = "Win10_2004_updates.csv"
+$2009 = "Win10_2009_updates.csv"
+$Win12r2 = "Win12r2_updates.csv"
+$Win16 = "Win16_updates.csv"
+$win19 = "Win19_updates.csv"
+$MS_Intel = "intel_updates.csv"
+$Win10 = "Win10_updates.csv"
+
+New-Item -Path $location -ItemType "Directory"
+
+Get-MSCatalogUpdate -Search "Cumulative Update for Windows 10 Version 1809" | Export-csv -path "$location\$1809" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Update for Windows 10 (1809)" | Export-csv -path "$location\$1809" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Cumulative Update for Windows 10 Version 1903" | Export-csv -path "$location\$1903" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Update for Windows 10 (1903)" | Export-csv -path "$location\$1903" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Cumulative Update for Windows 10 Version 1909" | Export-csv -path "$location\$1909" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Update for Windows 10 (1909)" | Export-csv -path "$location\$1909" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Cumulative Update for Windows 10 Version 2004" | Export-csv -path "$location\$2004" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Update for Windows 10 (2004)" | Export-csv -path "$location\$2004" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Cumulative Update for Windows 10 Version 2009" | Export-csv -path "$location\$2009" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Update for Windows 10 (2009)" | Export-csv -path "$location\$2009" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Cumulative Update for Windows Server 2012 R2" | Export-csv -path "$location\$Win12r2" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Update for Server 2012 R2" | Export-csv -path "$location\$Win12r2" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Cumulative Update for Windows Server 2016" | Export-csv -path "$location\$Win16" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Update for Server 2016" | Export-csv -path "$location\$Win16" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Cumulative Update for Windows Server 2019" | Export-csv -path "$location\$Win19" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Update for Windows Server 2019" | Export-csv -path "$location\$Win19" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Microsoft Defender Antivirus" | Export-Csv -path "$location\$MS_Intel" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Security Intelligence Update" | Export-Csv -path "$location\$MS_Intel" -NoTypeInformation -Append
+
+Get-MSCatalogUpdate -Search "Windows 10" | Export-Csv -path "$location\$Win10" -NoTypeInformation -Append
+
+#Get-MSCatalogUpdate -Search "Cumulative Update for Windows 10 Version 1909" | Export-csv -path "C:\data\endpoint\Win10_1909_updates.csv" -NoTypeInformation -Append
+
+#Get-MSCatalogUpdate -Search "Security Update for Windows 10 (1909)" | Export-csv -path "C:\data\endpoint\Win10_1909_updates.csv" -NoTypeInformation -Append
+
+#Change XXXX to the version number
+
