1bde7be1459e6e6459a7d96eee62d8adf8a350c3
rustyrat21 <hampton.jp@gmail.com>
2022-05-13 15:26:59 -0400

    Update Update_Installed_Modules.ps1

--- Update_Installed_Modules.ps1
+++ Update_Installed_Modules.ps1
@@ -7,3 +7,17 @@ Foreach($mod in $mods){
 }
 
 Update-Module -name ImportExcel 
+
+Update-Module -Name MSCatalog | Install-Module -Scope CurrentUser -Force -Credential $cred
+
+Find-Module -Name Azure | Install-Module -Scope CurrentUser -Force -Credential $cred
+
+Find-Module -name azuread | Install-Module -Scope CurrentUser -Force -Credential $cred
+
+Find-Module -name MSGraph | Install-Module -Scope CurrentUser -Force -Credential $cred
+
+Find-Module -name ExchangeOnlineManagement | Install-Module -Scope CurrentUser -Force -Credential $cred
+
+Find-Module -Name Microsoft.Graph.Intune | Install-Module -Scope CurrentUser -Force -Credential $cred
+
+Update-Module -Name Microsoft.Online.SharePoint.PowerShell
